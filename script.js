236451c2f04d12460a2f0290742fe6bf42b77b1e
lukevanlint <lukevanlint@gmail.com>
2026-04-15 00:42:38 +0100

    run

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sidebarContainer = document.getElementById("sidebar-container");
   const currentPath = window.location.pathname;
-  const currentFile = currentPath.split("/").pop();
 
   let sidebarPath = "sidebar.html";
 
   if (currentPath.includes("/calculators/")) {
     const depthAfterCalculators = currentPath.split("/calculators/")[1].split("/").length - 1;
     sidebarPath = "../".repeat(depthAfterCalculators + 1) + "sidebar.html";
+  }
+
+  if (currentPath.includes("/calculators/electrical/")) {
+    document.body.classList.add("electrical");
+  } else if (currentPath.includes("/calculators/public-health/")) {
+    document.body.classList.add("public-health");
+  } else if (currentPath.includes("/calculators/mechanical/")) {
+    document.body.classList.add("mechanical");
   }
 
   if (sidebarContainer) {
@@ -31,14 +38,6 @@ document.addEventListener("DOMContentLoaded", function () {
             link.classList.add("active");
           }
         });
-
-        if (currentPath.includes("/calculators/electrical/")) {
-          document.body.classList.add("electrical");
-        } else if (currentPath.includes("/calculators/public-health/")) {
-          document.body.classList.add("public-health");
-        } else if (currentPath.includes("/calculators/mechanical/")) {
-          document.body.classList.add("mechanical");
-        }
 
         sidebarContainer.classList.add("sidebar-ready");
       })
